0896f51c402eac4f7ba926f4d7b550f1a1c0ce09
Andrii Gadar <gadar.andre@gmail.com>
2024-05-23 19:35:59 +0300

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1280px',
+      smOnly: { max: '767.98px' },
+      mdOnly: { min: '768px', max: '1279.98px' },
+      notLg: { max: '1279.98px' },
+    },
+
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '2.125rem',
+        lg: '2rem',
+      },
+    },
+
+    extend: {
+      colors: {
+        bg1: '#FFFFFF',
+        bg2: '#FBF3EB',
+      },
+
+      fontFamily: {
+        geologica: ['Geologica', 'sans-serif'],
+      },
+
+      fontSize: {
+        small: ['14px', '1.5'],
+        normal: ['16px', '1.5'],
+        large: ['18px', '1.5'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
