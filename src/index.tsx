16a73cc45068504b0dfa37816fb2bfb6d7701de9
Andrii Gadar <gadar.andre@gmail.com>
2024-05-27 23:12:47 +0300

    added BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -12,8 +13,10 @@ import { persistor, store } from './redux/store';
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
+      <PersistGate loading={'Loading...'} persistor={persistor}>
+        <BrowserRouter basename='/luna_edge_quiz'>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </StrictMode>,
